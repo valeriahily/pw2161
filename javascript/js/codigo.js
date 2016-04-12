53e22eec983ac7449b416e5a61ebe0cf60ee90ad
valeriahily <valeria.riodel@hotmail.com>
2016-04-12 10:59:16 -0600

    Archivos JS, funcion anonima y consola

---
+++ javascript/js/codigo.js
@@ -0,0 +1,9 @@
+		var botonClic = function()
+		{
+			alert("Clic del botón");
+		}
+
+		var nombre = "Martin";
+		document.write("<strong>JS en acción</strong>"+" "+nombre);
+		console.log("A punto de lanzar un mensaje");
+		alert("Mensaje por pantalla");
