2264251a4691c7da48ee1e4a0f18f50d8ca01151
valeriahily <valeria.riodel@hotmail.com>
2016-04-20 10:58:40 -0600

    Gato disque empezado

---
+++ gato/js/gato.js
@@ -0,0 +1,20 @@
+var turn ="";
+//var cont=0;
+
+function juega()
+{
+	var text="";
+	var x=document.getElementbyId('td1').value;
+	if(x==null)
+	{
+		if(cont%2==0)
+		{
+			document.getElementbyId('td1').innerHTML=="X";
+		}
+		else
+		{
+			document=getElementbyId('td1').innerHTML=="O";
+		}	
+	}
+
+}
