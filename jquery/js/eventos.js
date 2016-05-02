d1941323361fc109bdbcf24fc435df22bb53b4a0
valeriahily <valeria.riodel@hotmail.com>
2016-05-02 11:02:40 -0600

    Teclas input ej JQuery

--- jquery/js/eventos.js
+++ jquery/js/eventos.js
@@ -10,8 +10,23 @@ var inicio = function()
 		$(".anuncioWeb").html("Clic del Botón");
 		$(".anuncioWeb").append("Clic del Botón");
 	}
+
+	var clicBoton2 = function ()
+	{
+		alert("Boton 2")
+	}
+
+	var teclaUnInput = function(tecla)
+	{
+		if(tecla.which == 13)
+		{
+			$("#otroInput").focus(); //Que se posicione en oroInput
+		}
+	}
 	//Preparar los eventos de todos mis objetos
-	$("#miBoton").on("click",clicBoton); //Estamos programando el evento clic del boton
+	$("#miBoton").off("click",clicBoton); //Estamos programando el evento clic del boton activado 
+	$("#miBoton").on("click",clicBoton2); //Desactivado
+	$("#unInput").on("keypress",teclaUnInput); //Keypresss nos lleva a la tecla seleccionada
 }
 //Main
 $(document).on("ready",inicio); 
