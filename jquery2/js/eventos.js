9495a65186aae3d58418fe8ab201d748fcfa8252
valeriahily <valeria.riodel@hotmail.com>
2016-05-12 11:02:14 -0600

    Iniciar AJAX para validar usuario. Archivos PHP vacio

--- jquery2/js/eventos.js
+++ jquery2/js/eventos.js
@@ -18,18 +18,40 @@ var iniciaApp = function()
 			alert("El clave no deber ser vacío");
 			$("#txtClave").focus();
 		}
-		//2.-Verificar usuario y contraseña
-		if(usuario == "pw" && clave == "1234")
-		{
-			//alert("Bienvenido "+usuario);
-			//Dar entrada al usuario
-			$("#datosUsuario").hide(); //Escondemos
-			$("nav").show("slow"); //Mostramos
-		}
-		else
-		{
-			alert("Usuario y/o contraseña incorrecta(s)");
-		}
+		// ** 2.-Verificar usuario y contraseña SIN PHP **
+		// if(usuario == "pw" && clave == "1234")
+		// {
+		// 	//alert("Bienvenido "+usuario);
+		// 	//Dar entrada al usuario
+		// 	$("#datosUsuario").hide(); //Escondemos
+		// 	$("nav").show("slow"); //Mostramos
+		// }
+		// else
+		// {
+		// 	alert("Usuario y/o contraseña incorrecta(s)");
+		// }
+		var parametros = "accion=validaEntrada"+
+						 "&usuario="+usuario+
+						 "&clave="+clave+
+						 "&id="+Math.random(); //Para no utilizar el cache
+		$.ajax({
+			beforeSend: function(){
+				console.log("Validar al usuario");
+			},
+			cache: false,
+			type: "POST", //Enviar datos
+			dataType: "json", //Como recibir los datos
+			ulr:"php/funciones.php" //Donde los vamos a mandar
+			data:parametros,
+			success: function(response){
+
+			},
+			error: function(xhr,ajax0ptionx,thrownError){
+				console.log("Algo salió mal");
+			}
+
+		});
+
 		console.log("Se disparó el submit");
 	}
 	$("#frmValidaEntrada").on("submit",validarEntrada);
